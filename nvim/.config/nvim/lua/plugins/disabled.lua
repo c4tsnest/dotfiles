cdcff74b83e7dcc485f40f07fe356aee8fba58e7
TomoyaOTA <tota6953@g.ecc.u-tokyo.ac.jp>
2025-09-10 23:17:44 +0900

    blink.cmpに乗り換え

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
-  { "L3MON4D3/LuaSnip", enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
+	-- { "L3MON4D3/LuaSnip", enabled = false },
+	-- { "rafamadriz/friendly-snippets", enabled = false },
+	-- { "github/copilot", enabled = false },
+
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,49 @@
 return {
 	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+
+			-- C/C++
+			lspconfig.clangd.setup({
+				cmd = { "clangd" },
+				filetypes = { "c", "cpp", "objc", "objcpp" },
+				-- capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					local opts = { noremap = true, silent = true }
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+				end,
+			})
+
+			-- Python
+			lspconfig.pyright.setup()
+
+			-- Lua (Neovim 用)
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+						telemetry = { enable = false },
+						format = {
+							enable = true,
+							defaultConfig = { indent_style = "space", indent_size = "2" },
+						},
+					},
+				},
+			})
+		end,
+	},
+
+	-- Mason
+	{
 		"williamboman/mason.nvim",
 		version = "1.*",
 		config = function()
@@ -14,48 +58,6 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = { "pyright", "lua_ls", "clangd" },
 				automatic_installation = true,
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-
-			-- C/C++
-			lspconfig.clangd.setup({
-				cmd = { "clangd" },
-				filetypes = { "c", "cpp", "objc", "objcpp" },
-				capabilities = vim.lsp.protocol.make_client_capabilities(),
-				on_attach = function(client, bufnr)
-					local opts = { noremap = true, silent = true }
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
-					-- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-				end,
-			})
-
-			-- Python
-			lspconfig.pyright.setup({})
-
-			-- Lua (Neovim 用)
-			lspconfig.lua_ls.setup({
-				settings = {
-					Lua = {
-						runtime = { version = "LuaJIT" },
-						diagnostics = { globals = { "vim" } },
-						workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-						telemetry = { enable = false },
-						format = {
-							enable = true,
-							defaultConfig = {
-								indent_style = "space",
-								indent_size = "2",
-							},
-						},
-					},
-				},
 			})
 		end,
 	},
